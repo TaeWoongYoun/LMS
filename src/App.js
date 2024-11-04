e8e0812646abd992c0c14b9dde1dbd76e99f9629
xodnd0507 <24457545yong@gmail.com>
2024-11-04 11:57:03 +0900

    docs : Upload file..

--- src/App.js
+++ src/App.js
@@ -12,24 +12,26 @@ function App() {
     return (
         <div className="App">
             <h1>모듈 목록</h1>
-            <table className="module-table">
-                <thead>
-                    <tr>
-                        <th>난이도</th>
-                        <th>폴더명</th>
-                        <th>세부 폴더명</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {iframeData.map((item, index) => (
-                        <tr key={index} onClick={() => handleSelect(item)} className="table-row">
-                            <td>{item.level}</td>
-                            <td>{item.module}모듈</td>
-                            <td>{item.name}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            <div className='table-area'>
+              <table className="module-table">
+                  <thead>
+                      <tr>
+                          <th>난이도</th>
+                          <th>폴더명</th>
+                          <th>세부 폴더명</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      {iframeData.map((item, index) => (
+                          <tr key={index} onClick={() => handleSelect(item)} className="table-row">
+                              <td>{item.level}</td>
+                              <td>{item.module}모듈</td>
+                              <td>{item.name}</td>
+                          </tr>
+                      ))}
+                  </tbody>
+              </table>
+            </div>
 
             {selected && (
                 <div className='iframe-area'>
