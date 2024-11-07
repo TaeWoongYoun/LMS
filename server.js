52e5463b62645c04a377dd647a009c18104cd404
xodnd0507 <24457545yong@gmail.com>
2024-11-08 08:29:08 +0900

    test : Test server

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+
 const app = express();
 
-app.use(cors()); // CORS 미들웨어 추가
+app.use(cors());
 app.use(express.json());
 
-app.post('/insert', (req, res) => {
-    const { module, level, html, css, js } = req.body;
-
-    // 데이터베이스 연결 및 삽입 로직 추가
-    console.log('Received data:', module, level, html, css, js);
-    res.status(200).send('Data inserted successfully');
+app.get('/', (req, res) => {
+    res.send('Node server is running!');
 });
 
 app.listen(3001, () => {
