338f00736e5beb5790de6f1cf3eb126a4e0a335f
xodnd0507 <24457545yong@gmail.com>
2024-11-11 16:22:33 +0900

    feat : Create welcome page

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import './styles/WelcomePage.css';
 
 function WelcomePage() {
     return (
-        <div>
-            <h1>Welcome Page</h1>
-            <p>Welcome to the home page!</p>
+        <div className="welcome-container">
+            <main className="main-content">
+                <h1 className="welcome-title">인사말</h1>
+                <div className="message-box">
+                    <p className="message-text">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa soluta repudiandae consequatur asperiores ipsum magni, delectus, nihil quam excepturi libero quia ut magnam, sapiente aliquid porro aspernatur deleniti dolores. Corporis.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa soluta repudiandae consequatur asperiores ipsum magni, delectus, nihil quam excepturi libero quia ut magnam, sapiente aliquid porro aspernatur deleniti dolores. Corporis.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa soluta repudiandae consequatur asperiores ipsum magni, delectus, nihil quam excepturi libero quia ut magnam, sapiente aliquid porro aspernatur deleniti dolores. Corporis.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa soluta repudiandae consequatur asperiores ipsum magni, delectus, nihil quam excepturi libero quia ut magnam, sapiente aliquid porro aspernatur deleniti dolores. Corporis.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa soluta repudiandae consequatur asperiores ipsum magni, delectus, nihil quam excepturi libero quia ut magnam, sapiente aliquid porro aspernatur deleniti dolores. Corporis.
+                    </p>
+                </div>
+            </main>
         </div>
     );
 }
